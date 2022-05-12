4782be39e18eea959db679dda613cb9ef898f35b
Victor Serapiglia <victorserapiglia@hotmail.com>
2022-05-12 19:58:18 +0200

    rails new

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,3 @@
 Rails.application.routes.draw do
-  get 'ask', to: 'questions#ask'
-  get 'answer', to: 'questions#answer'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
